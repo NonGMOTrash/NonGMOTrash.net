e11330d1ea10688a71226b8a976e5bbbb50c2f93
NonGMOTrash <nongmotrash@gmail.com>
2023-06-03 23:15:15 -0700

    updated backrooms

--- backrooms/4/script.js
+++ backrooms/4/script.js
@@ -1,1 +1,14 @@
-//window.location.href = "https://www.reddit.com/r/dontgohere/";
+var o = 0;
+
+function Activate() {
+	document.getElementById("prize").style.display = "none";
+	document.getElementById("bridgets").style.display = "inline";
+    document.body.style.backgroundImage = "url('bridget.png')";
+	
+	function test() {
+		o += 10;
+		document.body.style.backgroundPosition = o + "px " + o + "px";
+    }
+    
+	setInterval(test, 1000/60);
+}
