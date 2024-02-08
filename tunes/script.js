991a5c7c45d9358e3fdacc7a0e3d563892b97a54
NonGMOTrash <nongmotrash@gmail.com>
2024-02-08 02:05:50 -0800

    feb 7

--- tunes/script.js
+++ tunes/script.js
@@ -12,7 +12,8 @@ function ShowCategory(category)
 	document.getElementById(category).style.display = "block";
 }
 
-function Play(songTitle, songUrl)
+function Play(songTitle, songID)
 {
-
+	document.getElementById("song-source").src = "https://docs.google.com/uc?export=download&id=1vFNQ7u17O9RDrL_j5wbYTu8NtkEEYHRJ";
+	document.getElementById("song").play();
 }
