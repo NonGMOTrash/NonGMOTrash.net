d103bd04344892f95276850e90d1fb1e9ed2cf69
NonGMOTrash <nongmotrash@gmail.com>
2024-02-11 21:36:18 -0800

    feb 11

--- tunes/script.js
+++ tunes/script.js
@@ -12,8 +12,40 @@ function ShowCategory(category)
 	document.getElementById(category).style.display = "block";
 }
 
-function Play(songTitle, songID)
-{
-	document.getElementById("song-source").src = "https://docs.google.com/uc?export=download&id=1vFNQ7u17O9RDrL_j5wbYTu8NtkEEYHRJ";
-	document.getElementById("song").play();
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+		height: '300',
+		width: '300',
+		videoId: '',
+		playerVars: {
+			'playsinline': 1
+		}/*,
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}*/
+	});
+}
+
+// function onPlayerReady(event) {
+// 	event.target.playVideo();
+// }
+
+// function onPlayerStateChange(event) {
+// 	console.log(player.getVideoUrl());
+// }
+
+function playSong(ID) {
+	document.getElementById("player-holder").style.opacity = 100;
+	player.loadVideoById(ID, 0);
+	player.playVideo();
 }
