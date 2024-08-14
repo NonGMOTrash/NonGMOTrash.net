9a1187d0478fc5b7eaa1abf08557f6c3ee0599aa
NonGMOTrash <nongmotrash@gmail.com>
2024-08-13 23:32:01 -0700

    added old news

--- home/script.js
+++ home/script.js
@@ -19,7 +19,7 @@ const textRainbow = [
 
 const DAY = 1000 * 60 * 60 * 24;
 
-var lastUpdate = new Date("2024-8-9").getTime();
+var lastUpdate = new Date("2024-8-13").getTime();
 var currentDay = new Date().getTime();
 var days = Math.floor(Math.abs((currentDay - lastUpdate) / DAY))
 var dayCount = document.getElementById("day-count");
