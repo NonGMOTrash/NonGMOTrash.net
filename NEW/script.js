9c450b61c96c0c78952875d9f4612bd5faa2a31d
NonGMOTrash <nongmotrash@gmail.com>
2025-02-16 20:09:39 -0800

    project progress: added statuses + link buttons

--- NEW/script.js
+++ NEW/script.js
@@ -23,13 +23,30 @@ setTimeout(
 
 window.onload = function()
 {
+	const colorActive = "#a2fad8";
+	const colorBackburner = "#e8b66b";
+	const colorHiatus = "#943b63";
+
 	var projects = document.getElementById("progress").children;
 
 	for (var i = 0; i < projects.length; i++)
 	{
+		if (projects[i].className != "project") {
+			break;
+		}
+
 		var completion = projects[i].getAttribute("completion");
+		var color = colorActive;
+		if (projects[i].getAttribute("status") == "backburner") {
+			color = colorBackburner;
+		} else if (projects[i].getAttribute("status") == "hiatus") {
+			color = colorHiatus;
+		}
+
+		projects[i].style.borderColor = color;
 		projects[i].style.background = ( //          \/ convert % into degrees
-				"conic-gradient(var(--color-highlight) "+completion*3.6+"deg, var(--color-fg) 0deg");
+				"conic-gradient("+color+" "+completion*3.6+"deg, var(--color-fg) 0deg");
+		projects[i].setAttribute("title", projects[i].getAttribute("status"));
 
 		var text = projects[i].querySelector(".text");
 		text.textContent += "\n("+completion+"%)";
