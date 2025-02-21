4722fced2392a06e7345ba2a03a55fa8300a0869
NonGMOTrash <nongmotrash@gmail.com>
2025-02-20 20:27:04 -0800

    restructured and rewrote writing pages

--- NEW/writing/writing.js
+++ NEW/writing/writing.js
@@ -1,34 +1,38 @@
 window.onload = function(){
 	// GENERATE DOCUMENT OUTLINE
 
+	const outline = document.getElementById("outline");
 	const headers = document.querySelectorAll("h1, h2");
 
-	for (var i = 0; i < headers.length; i++)
+	if (headers.length > 1)
 	{
-		headers[i].id = "header-" + i.toString();
-	}
-
-	const outline = document.getElementById("outline");
-	for (var i = 0; i < headers.length; i++)
-	{
-		const element = document.createElement("a");
-		element.innerText = i.toString() + ". ";
-		if (i != 0) {
-			element.innerText += headers[i].innerText;
-		} else {
-			element.innerText += "Intro";
+		for (var i = 0; i < headers.length; i++)
+		{
+			headers[i].id = "header-" + i.toString();
 		}
-		element.href = "#header-" + i.toString();
 
-		outline.appendChild(element);
+		for (var i = 0; i < headers.length; i++)
+		{
+			const element = document.createElement("a");
+			if (i != 0) {
+				element.innerText = headers[i].innerText;
+			} else {
+				element.innerText = "Intro";
+			}
+			element.href = "#header-" + i.toString();
+
+			outline.appendChild(element);
+		}
+	} else {
+		outline.style.display = "none";
 	}
 
 	// INSERT FOOTER BUTTONS
 	document.getElementById("footer").innerHTML = `
 		<p>
-			<a href="../index.html">read some more?</a>
+			<a href="../../index.html">read some more?</a>
 			//
-			<a href="../../index.html">go home</a>
+			<a href="../../../index.html">go home</a>
 		</p>
 	`;
 }
