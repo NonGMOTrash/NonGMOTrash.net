bfa5a4f337c031f8d3f3eb3935d4e4a4131acb08
NonGMOTrash <nongmotrash@gmail.com>
2025-04-13 17:33:45 -0700

    ABOUT: made favorite games page + changed name-styling

---
+++ NEW/about/favs/books/script.js
@@ -0,0 +1,7 @@
+function updateDescription(game) {
+	switch(game) {
+		case "mistborn":
+			document.getElementById("thoughts").innerHTML = "Mistborn <i>(Brandon Sanderson)</i><br>i was really impressed by every aspect of this book. despite being the first entry of a triology, it stands perfectly on its own as a great story which i admire. the magic system is the most interesting i've seen and the world felt like the logical outcome of that system - it was really well done. what really elevates this book for me though were the characters, vin's arc in particular was really memorable for me.";
+			break;
+	}
+}
---
+++ NEW/about/favs/movies/script.js
@@ -0,0 +1,10 @@
+function updateDescription(game) {
+	switch(game) {
+		case "everything":
+			document.getElementById("thoughts").innerHTML = "Everything Everywhere All At Once<br>everything about this movie is really well done -- i usually prefer animated anything over live action because im a little goblin, but this film managed to break past that and become my favorite. i found the sentiment really touching, and i thought the way the multiverse / sensory overload was portrayed was very well done.";
+			break;
+		case "parasite":
+			document.getElementById("thoughts").innerHTML = "Parasite<br>going in, i thought this was going to be about an actual parasite... so needless to say i was definitely taken for a ride watching this absolutely insane movie. there were so many moments where i was just <i>floored</i> at what was happening. it was absolutely hilarious at times (especially the art therapy), but it also definitely has some more depth to it. i really enjoyed the films commentary on class divides, and i thought it was portrayed very well (the 'smell' motif in particular was really memorable).";
+			break;
+	}
+}
