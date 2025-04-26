7814eb703d698d369981fdc2bcd4864632facfb5
NonGMOTrash <nongmotrash@gmail.com>
2025-04-25 17:25:58 -0700

    GLOBAL: added simple page load animation

--- NEW/about/script.js
+++ NEW/about/script.js
@@ -1,5 +1,11 @@
 function toggleSection(sectionId)
 {
+	console.log(sectionId);
+	if (sectionId === "")
+	{
+		return;
+	}
+
 	let section = document.getElementById(sectionId);
 	let sectionHeight = section.getBoundingClientRect().height;
 
@@ -13,3 +19,6 @@ function toggleSection(sectionId)
 		// TODO: this value should be dynamically chosen based on the height of the section. if the section is too short, closing it will have a weird delay because it starts from 130% down, rather than the bottom of the section. and if the section is longer than 130%, then the bottom will be cut off :(
 	}
 }
+
+toggleSection((window.location.hash).slice(1)); // slice() is needed to remove the first character (#) from the beginning
+
