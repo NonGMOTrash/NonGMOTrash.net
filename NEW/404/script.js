76936a5ad53bd575b5cb3f1a9bc447c3a310307e
NonGMOTrash <nongmotrash@gmail.com>
2025-04-28 19:29:52 -0700

    HOME: restored jimmy + 404: fixed clicking link before letter falls

--- NEW/404/script.js
+++ NEW/404/script.js
@@ -4,32 +4,40 @@ var portal;
 
 var hasFallen = false;
 
-window.onload = function() {
-	loose = document.getElementById("loose");
-	letter = document.getElementById("letter");
-	portal = document.getElementById("portal");
+loose = document.getElementById("loose");
+letter = document.getElementById("letter");
+portal = document.getElementById("portal");
 
-	loose.onclick = function() {
-		if (!hasFallen) {
-			letter.animate(
-				[
-					{ transform: 'rotate(4deg) translate(0, 0)' },
-					{ transform: 'rotate(4deg) translate(0%, 500%)' }
-				],
-				{
-					duration: 1000,
-					easing: "ease-in",
-					fill: "forwards"
-				}
-			);
+loose.onclick = function()
+{
+	if (!hasFallen) {
+		letter.animate(
+			[
+				{ transform: 'rotate(4deg) translate(0, 0)' },
+				{ transform: 'rotate(4deg) translate(0%, 500%)' }
+			],
+			{
+				duration: 1000,
+				easing: "ease-in",
+				fill: "forwards"
+			}
+		);
 
-			hasFallen = true;
+		hasFallen = true;
 
-		}
 	}
-
-	setTimeout(function() {
-		portal.style.pointerEvents = "auto";
-	}, 500);
 }
+
+portal.onclick = function()
+{
+	if (hasFallen)
+	{
+		window.location.href = "../backrooms/index.html";
+	}
+}
+
+setTimeout(function() {
+	portal.style.pointerEvents = "auto";
+}, 500);
+
 
