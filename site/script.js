a4faac8251a5b28388fa34472bf8094fd7eb2d9d
NonGMOTrash <nongmotrash@gmail.com>
2025-06-03 19:12:26 -0700

    HOME: added warning if JS is disabled

--- site/script.js
+++ site/script.js
@@ -20,38 +20,39 @@ if (document.documentElement.scrollWidth > 1500)
 	4000);
 }
 
+// nongmotrash-js-warning
+
+document.getElementById("nongmotrash-js-warning").style.display = "none";
+
 // project progress
 
-window.onload = function()
+const colorActive = "#a2fad8";
+const colorBackburner = "#e8b66b";
+const colorHiatus = "#943b63";
+
+var projects = document.getElementById("progress").children;
+
+for (var i = 0; i < projects.length; i++)
 {
-	const colorActive = "#a2fad8";
-	const colorBackburner = "#e8b66b";
-	const colorHiatus = "#943b63";
-
-	var projects = document.getElementById("progress").children;
-
-	for (var i = 0; i < projects.length; i++)
-	{
-		if (projects[i].className != "project") {
-			break;
-		}
-
-		var completion = projects[i].getAttribute("completion");
-		var color = colorActive;
-		if (projects[i].getAttribute("status") == "backburner") {
-			color = colorBackburner;
-		} else if (projects[i].getAttribute("status") == "hiatus") {
-			color = colorHiatus;
-		}
-
-		projects[i].style.borderColor = color;
-		projects[i].style.background = ( //          \/ convert % into degrees
-				"conic-gradient("+color+" "+completion*3.6+"deg, var(--color-fg) 0deg");
-		projects[i].setAttribute("title", projects[i].getAttribute("status"));
-
-		var text = projects[i].querySelector(".text");
-		text.textContent += "\n("+completion+"%)";
+	if (projects[i].className != "project") {
+		break;
 	}
+
+	var completion = projects[i].getAttribute("completion");
+	var color = colorActive;
+	if (projects[i].getAttribute("status") == "backburner") {
+		color = colorBackburner;
+	} else if (projects[i].getAttribute("status") == "hiatus") {
+		color = colorHiatus;
+	}
+
+	projects[i].style.borderColor = color;
+	projects[i].style.background = ( //          \/ convert % into degrees
+			"conic-gradient("+color+" "+completion*3.6+"deg, var(--color-fg) 0deg");
+	projects[i].setAttribute("title", projects[i].getAttribute("status"));
+
+	var text = projects[i].querySelector(".text");
+	text.textContent += "\n("+completion+"%)";
 }
 
 // chattable
