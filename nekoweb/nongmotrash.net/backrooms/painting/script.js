ebcc0aa002d1c8e50c57f2649b56c7610344dad5
NonGMOTrash <nongmotrash@gmail.com>
2025-08-08 21:19:26 -0700

    HOME: hacky chattable fix?
    added beta to URL

--- nekoweb/nongmotrash.net/backrooms/painting/script.js
+++ nekoweb/nongmotrash.net/backrooms/painting/script.js
@@ -2,9 +2,9 @@ function Warp() {
 	var i = Math.floor(Math.random() * 10);
 	
 	if (i == 0) {
-		window.location.href = "../backroom/index.html";
-	} else if (i == 1) {
 		window.location.href = "../backroom2/index.html";
+	} else if (i == 1) {
+		window.location.href = "../backroom3/index.html";
 	} else if (i == 2) {
 		window.location.href = "../sans_room/index.html";
 	} else if (i == 3) {
@@ -16,7 +16,7 @@ function Warp() {
 	} else if (i == 6) {
 		window.location.href = "../rickroll/index.html";
 	} else if (i == 7) {
-		window.location.href = "../poolroom/index.html";
+		window.location.href = "../two/index.html";
 	} else if (i == 8) {
 		window.location.href = "../close/index.html";
 	} else if (i == 9) {
